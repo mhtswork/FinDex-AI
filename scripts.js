0b14e5b784d61d6064a1a411bf0ffa55209d9afd
mhtswork <158544131+mhtswork@users.noreply.github.com>
2026-08-04 21:03:45 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-const tallyContainer = document.querySelector('.tally-widget-container');
-
-if (tallyContainer) {
-  const script = document.createElement('script');
-  script.src = 'https://tally.so/widgets/embed.js';
-  script.async = true;
-  document.body.appendChild(script);
-}
+// The contact form is embedded directly in the page for reliable rendering.
